dc6fbae9bde1e439aad623dc043ce2c997f1abdf
BoujeeWhoCodes <145911615+BoujeeWhoCodes@users.noreply.github.com>
2023-12-07 17:54:33 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector(".weather-app-city");
-  cityElement.textContent = searchInput.value;
+  let cityElements = document.querySelectorAll(".weather-app-city");
+  
+  // Assuming there might be multiple instances of city names
+  cityElements.forEach(cityElement => {
+    cityElement.textContent = searchInput.value;
+  });
 }
 
 let searchFormElement = document.querySelector("#search-form");
-searchFormElement.addEventListener("
+searchFormElement.addEventListener("submit", handleSearchSubmit);
